cf5579cd56f3c88a88fecc5ef67d9d804f22cb73
hezhongxian <jhonhe7night@outlook.com>
2024-09-05 16:40:53 +0800

    update - jwt

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,86 +1,116 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"net/http"
 	"strings"
-	"time"
 	"web-server/utils"
 	"web-server/utils/errmsg"
 )
 
 type JWT struct {
-	JwtKey []byte
-}
-
-type Claims struct {
-	Username string `json:"username"`
-	jwt.RegisteredClaims
+	JWTKey []byte
 }
 
 func NewJWT() *JWT {
 	return &JWT{
-		JwtKey: []byte(utils.JwtKey),
+		JWTKey: []byte(utils.JwtKey),
 	}
 }
 
-func (j *JWT) CreateToken(claims Claims) (string, error) {
+type MyClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+var (
+	TokenExpired     = errors.New("token is expired")
+	TokenNotValidYet = errors.New("token not active yet")
+	TokenMalformed   = errors.New("that's not even a token")
+	TokenInvalid     = errors.New("couldn't handle this token")
+)
+
+// CreateToken sha256生成Token
+func (j *JWT) CreateToken(claims MyClaims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(j.JwtKey)
+	return token.SignedString(j.JWTKey)
 }
 
-func (j *JWT) ParseToken(tokenString string) (*Claims, error) {
-	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return j.JwtKey, nil
+// ParseToken 解析Token
+func (j *JWT) ParseToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return j.JWTKey, nil
 	})
-	if err != nil {
-		return nil, err
+	// 验证token
+	if token.Valid {
+		return nil
+	} else if errors.Is(err, jwt.ErrTokenMalformed) {
+		return TokenMalformed
+	} else if errors.Is(err, jwt.ErrTokenExpired) {
+		return TokenExpired
+	} else if errors.Is(err, jwt.ErrSignatureInvalid) {
+		return TokenInvalid
+	} else {
+		return TokenNotValidYet
 	}
-	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
-		return claims, nil
-	}
-	return nil, err
 }
 
-func JWTAuth() gin.HandlerFunc {
+// JwtToken jwt 中间件
+func JwtToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenHeader := c.Request.Header.Get("Authorization")
-		if tokenHeader == "" {
+		var code int
+		tokenString := c.Request.Header.Get("Authorization")
+		if tokenString == "" {
+			code = errmsg.ErrorTokenExist
 			c.JSON(http.StatusOK, gin.H{
-				"status":  errmsg.ErrorTokenExist,
-				"message": errmsg.GetErrMsg(errmsg.ErrorTokenExist),
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
 			})
 			c.Abort()
 			return
 		}
-		parts := strings.SplitN(tokenHeader, " ", 2)
-		if !(len(parts) == 2 && parts[0] == "Bearer") {
+		checkToken := strings.Split(tokenString, " ")
+		if len(checkToken) == 0 {
 			c.JSON(http.StatusOK, gin.H{
-				"status":  errmsg.ErrorTokenTypeWrong,
-				"message": errmsg.GetErrMsg(errmsg.ErrorTokenTypeWrong),
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
 			})
 			c.Abort()
 			return
 		}
-		claims, err := NewJWT().ParseToken(parts[1])
+
+		if len(checkToken) != 2 || checkToken[0] != "Bearer" {
+			c.JSON(http.StatusOK, gin.H{
+				"status":  code,
+				"message": errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+		j := NewJWT()
+		// 解析 token
+		err := j.ParseToken(checkToken[1])
 		if err != nil {
+			if errors.Is(err, TokenExpired) {
+				c.JSON(http.StatusOK, gin.H{
+					"status":  errmsg.Error,
+					"message": "token is expired",
+					"data":    nil,
+				})
+				c.Abort()
+				return
+			}
+			// 其他错误
 			c.JSON(http.StatusOK, gin.H{
-				"status":  errmsg.ErrorTokenWrong,
-				"message": errmsg.GetErrMsg(errmsg.ErrorTokenWrong),
+				"status":  errmsg.Error,
+				"message": err.Error(),
+				"data":    nil,
 			})
 			c.Abort()
 			return
 		}
-		if time.Now().Unix() > claims.ExpiresAt.Unix() {
-			c.JSON(http.StatusOK, gin.H{
-				"status":  errmsg.ErrorTokenRuntime,
-				"message": errmsg.GetErrMsg(errmsg.ErrorTokenRuntime),
-			})
-			c.Abort()
-			return
-		}
-		c.Set("username", claims.Username)
 		c.Next()
 	}
 }
